e78d7976fa72d31ce506e41234f2e8d4db8c2a3d
si7o <sito.lc@gmail.com>
2013-09-30 09:35:24 +0200

    minor changes

--- app/view/comun/menu.php
+++ app/view/comun/menu.php
@@ -6,8 +6,8 @@
        		</a>	
    		</h2> 
    		<ul>
-   			<li><a class="flickr<?=$selected=='flickr'?' selected':''?>" href="/flickr" >for Flickr</a></li>
-   			<li id="btn_about"><a class="about<?=$selected=='about'?' selected':''?>" href="/about" >about</a></li>   			
+   			<li><a class="flickr<?=isset($selected)&&$selected=='flickr'?' selected':''?>" href="/flickr" >for Flickr</a></li>
+   			<li id="btn_about"><a class="about<?=isset($selected)&&$selected=='about'?' selected':''?>" href="/about" >about</a></li>   			
    			<li><a class="git" href="https://github.com/si7o/skybox" target="_blank">github</a></li>   			
    			<li id="btn_config"><a  class="conf" href="#" >config</a></li>	 	
    		</ul>
